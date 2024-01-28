5ecfd84df13886a5e00791c147202b06645370d7
Rafael <121521729+Rafaell77@users.noreply.github.com>
2024-01-28 15:15:24 -0300

    commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const form = document.querySelector("form");
+const tableBody = document.querySelector("table tbody");
+
+
+function addRow(nome, telefone) {
+  const newRow = tableBody.insertRow();
+  const nomeCell = newRow.insertCell();
+  const telefoneCell = newRow.insertCell();
+  nomeCell.textContent = nome;
+  telefoneCell.textContent = telefone;
+}
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const nome = event.target.nome.value;
+  const telefone = event.target.telefone.value;
+
+  
+  if (!nome || !telefone) {
+    alert("Por favor, preencha todos os campos."); 
+    return;
+  }
+
+  addRow(nome, telefone);
+
+  
+  event.target.reset();
+});
